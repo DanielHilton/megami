0a753ac2d099ebe6da8c35388aef04bf10b36440
Daniel Hilton <Daniel.hilton@bglgroup.co.uk>
2019-12-02 22:45:17 +0000

    Working now

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,22 @@
-<main>
-    <h1>Hello world!</h1>
-</main>
+<script>
+    export let version;
+    let name = "World";
+    let test = process.versions.electron;
+</script>
 
-<style>
+<style type="text/scss">
     h1 {
-        font-family: 'Comic Sans MS', sans-serif;
-        font-weight: 300;
         color: purple;
-        margin: 2em auto;
-        text-align: center;
+        span {
+            color: green;
+        }
     }
 </style>
+
+<h1>Hello <span>{name}</span>!</h1>
+<p>We are using Node.js version {process.versions.node},</p>
+<p>Chromium version {process.versions.chrome}</p>
+<p>and Electron version {process.versions.electron}.</p>
+<p>But most importantly <strong>Svelte</strong> version {version}!</p>
+<br>
+<p>This boilerplate, in addition to using Svelte with Electron, also uses <i>Node-Sass</i>!</p>
